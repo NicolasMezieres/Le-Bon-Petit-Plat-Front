43d78d5e1580206803718af6d7a13af924300860
NicolasMezieres <nicolas.mezieres73@gmail.com>
2024-10-14 04:19:54 +0200

    create directory

---
+++ src/Service/auth.ts
@@ -0,0 +1,3 @@
+import { signUpFormType } from "@/utils/type";
+
+export async function Signup(data: signUpFormType) {}
---
+++ src/utils/type.ts
@@ -0,0 +1,7 @@
+export type signUpFormType = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  password: string;
+};
