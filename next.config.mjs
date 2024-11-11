4dfc43400d7c9ab37fd9a56fc3c4f1877a4dbda6
NicolasMezieres <nicolas.mezieres73@gmail.com>
2024-11-11 17:37:02 +0100

    test change domain 3

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["https://lebonpetitplat-8af8a5c399a8.herokuapp.com"],
+    domains: ["https://lebonpetitplat-8af8a5c399a8.herokuapp.com/imageFile/"],
   },
 };
 
